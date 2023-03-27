7dd229af43031c91acf97332d114daeb6783b370
TheAkashRoy <9akash.roy@gmail.com>
2023-03-27 23:40:36 +0530

    req

--- Backend/app.py
+++ Backend/app.py
@@ -42,11 +42,3 @@ def data(uid):
 
 if __name__ == "__main__":
   app.run(debug=True)
-  # cluster = pymongo.MongoClient("mongodb+srv://emailforakashroy:nc3iezJqetnF0X0I@cluster0.hxo7td9.mongodb.net/?retryWrites=true&w=majority")
-  # text = "this is that"
-  # uid = shortuuid.ShortUUID().random(length=4)
-  # collection.insert_one({"_id":uid, "text":text})
-  # uid = "4zv8"
-  # t = collection.find_one({"_id":uid})
-  # print(t)
-  # collection.insert_one({"_id":0, "text":"this is the The"})
