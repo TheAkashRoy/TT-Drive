2ec5a64258de312758c9895cc61c13fe9efd57c3
TheAkashRoy <9akash.roy@gmail.com>
2023-03-29 09:38:32 +0530

    react files delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import Navbar from './components/Navbar';
 import './App.css';
+import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Textarea />
+    <Navbar />
+    <Textarea/>
     </>
   );
 }
