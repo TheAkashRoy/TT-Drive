557aee990258cbf68073caff4c2b12b3d505fc98
TheAkashRoy <9akash.roy@gmail.com>
2023-03-29 15:32:51 +0530

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Textarea from './components/Textarea';
+
 
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Textarea/>
     </>
   );
 }
