d6be457c4d3f97fed090a498abf3c1d21325507e
TheAkashRoy <9akash.roy@gmail.com>
2023-04-02 21:10:56 +0530

    access done

--- frontend/src/components/access.jsx
+++ frontend/src/components/access.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React,{useState}from 'react';
+import Button from "@mui/material/Button";
+import axios from 'axios';
+
 
 function Access() {
+  const [code,setCode]=useState("");
+  const [text,setText]=useState("");
+
+  const find = async (event) => {
+
+    event.preventDefault();
+    let url ="https://ttd-one.vercel.app/find/";
+    url=url.concat(code);
+    console.log(url);
+    axios.get(url)
+    .then(function (response) {
+      console.log(response.data);
+      setText(response.data);
+    })
+
+  };
   return (
     <div className='bg-[#EEEEEE]' >
       <div className='pt-24 px-10'>
       <p className='text-3xl mb-2 text-[#0B2447] font-semibold max-[640px]:text-2xl'>Enter code here</p><br/>
-      <input className='text-2xl mb-12 bg- rounded-lg p-1 text-[#0B2447]'></input>
-      
+      <input className='text-2xl mb-12 bg- rounded-lg p-1 text-[#0B2447]' value={code} onChange={(e) => setCode(e.target.value)}/>
+      <br />
+      <Button
+            variant="contained"
+            color="success"
+            size="large"
+            href="/access"
+            onClick={find}
+          >
+            Click here!
+          </Button>
    
       </div>
+      <div>
+        
+        {text}
+      </div>
     </div>
+    
   )
 }
 
--- frontend/src/components/generate.jsx
+++ frontend/src/components/generate.jsx
@@ -1,8 +1,22 @@
 import React,{useState} from 'react'
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import axios from 'axios';
+import Button from "@mui/material/Button";
+
 function Generate() {
     const [text,setText]=useState("");
+    const [code,setCode]=useState("");
+    const find = async (event) => {
+
+      event.preventDefault();
+      let url ="https://ttd-one.vercel.app/find/";
+      url=url.concat(code);
+      console.log(url);
+      axios.get(url)
+      .then(function (response) {
+        console.log(response.data);
+        setText(response.data);
+      })}
     // const [code,setCode]=useState("");
     // const [data,setData]=useState(
     //   {
@@ -55,6 +69,7 @@ function Generate() {
         }
       }).then(response => {
         console.log(response.data);
+        setCode(response.data);
       }).catch(error => {
         console.error(error);
       });
@@ -62,9 +77,34 @@ function Generate() {
     };
   return (
     <div className='bg-[#EEEEEE]' >
+      <div className='pt-24 px-10 text-center'>
+      <p className='text-3xl mb-2 text-[#0B2447] font-semibold max-[640px]:text-2xl'>If you have code enter below:</p>
+      <input className='text-2xl m-3 bg- rounded-lg p-1 text-[#0B2447]' value={code} onChange={(e) => setCode(e.target.value)}/>
+      <Button
+            variant="contained"
+            color="success"
+            size="large"
+            href="/access"
+            onClick={find}
+          >
+            Click here!
+          </Button>
+   
+      </div>
       <form  onSubmit={submit}>
-      <div className=' p-20 flex flex-col'>
-      <p className='text-4xl mb-4 text-[#393E46] font-semibold pt-3 max-[640px]:text-2xl'>Enter text here</p><br/>
+      <div className=' px-20 flex flex-col'>
+      {/* <p className="text-4xl mb-4 text-[#393E46] font-semibold pt-3 max-[640px]:text-2xl text-center text-black">
+          If you have code, then{" "}
+          <Button
+            variant="contained"
+            color="success"
+            size="large"
+            href="/access"
+          >
+            Click here!
+          </Button>
+        </p> */}
+      <p className='text-4xl mb-1 text-[#393E46] font-semibold pt-3 max-[640px]:text-2xl'>Enter text here</p><br/>
       <TextareaAutosize
       aria-label="minimum height"
       minRows={12}
@@ -75,7 +115,7 @@ function Generate() {
       value={text}
       onChange={(e) => setText(e.target.value)}
     />
-    <button className='text-2xl mb-12 bg-slate-200 rounded-xl p-2 text-[#393E46] w-36 font-semibold max-[640px]:text-lg shadow-xl' type='submit' >Upload</button>
+    <button className='text-2xl mb-4 bg-slate-200 rounded-xl p-2 text-[#393E46] w-36 font-semibold max-[640px]:text-lg shadow-xl' type='submit' >Upload</button>
     </div>
     </form>
     </div>
