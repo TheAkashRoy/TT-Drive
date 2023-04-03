fb84f4c95fb43255c3104fe0da811deda1aa45f8
TheAkashRoy <9akash.roy@gmail.com>
2023-04-03 21:52:22 +0530

    button

--- frontend/src/components/access.jsx
+++ frontend/src/components/access.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react'
 import axios from 'axios';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
 import Highlight from 'react-highlight'
 
 function Access() {
@@ -22,7 +21,7 @@ function Access() {
       <div className='pt-24 px-10 flex flex-col'>
       <p className='text-3xl mb-2 text-[#0B2447] font-semibold max-[640px]:text-2xl '>Enter code here</p><br/>
       <div className='flex flex-row'>
-      <input className='text-2xl mb-12 bg- rounded-lg p-1 text-[#0B2447] w-48' value={code} onChange={(e) => setCode(e.target.value)}></input>
+      <input className='text-2xl  bg- rounded-lg p-1 text-[#0B2447] w-48' value={code} onChange={(e) => setCode(e.target.value)}></input>
       <button className=' text-slate-800 font-semibold text-2xl bg-slate-100 mx-10 rounded-lg p-1 w-28 shadow-xl' onClick={find}>Submit</button>
       </div>
      {
